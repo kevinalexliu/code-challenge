5fab8390e766c9bf81bb4c2fef770fc31af3ec4d
Kevin Liu <kevinalexliu@gmail.com>
2021-07-05 13:49:01 -0700

    update css and add test

--- test/pages/api/create_new_account.test.ts
+++ test/pages/api/create_new_account.test.ts
@@ -16,4 +16,36 @@ describe('/api/create_new_account', () => {
       result: true,
     });
   });
+
+  test('return invalid username', async () => {
+    const { req, res } = mockRequest({
+      method: 'POST',
+      body: {
+        username: '',
+        password: 'password'
+      }
+    })
+    await createNewAccount(req, res);
+    expect(res._getStatusCode()).toBe(400);
+    expect(res._getJSONData()).toEqual({
+      result: false,
+      errors: {username: 'Username is not valid'}
+    });
+  });
+
+  test('return invalid password', async () => {
+    const { req, res } = mockRequest({
+      method: 'POST',
+      body: {
+        username: 'username',
+        password: ''
+      }
+    })
+    await createNewAccount(req, res);
+    expect(res._getStatusCode()).toBe(400);
+    expect(res._getJSONData()).toEqual({
+      result: false,
+      errors: {password: 'Password is not valid'}
+    })
+  })
 });
